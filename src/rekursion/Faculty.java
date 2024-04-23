5014c708e03c9fa59285bbeaabed2377305c5d57
LucaTröstl <lucsky.cr7@gmail.com>
2024-04-23 12:54:09 +0200

    zuig

--- src/rekursion/Faculty.java
+++ src/rekursion/Faculty.java
@@ -11,10 +11,22 @@ public class Faculty {
 		}
 	}
 	
+	public static int calculateBinomialkoeffizient(int n, int k) {
+		if()
+	}
+	
+	public static int pow(int b, int e) {
+		if(e==0) {
+			return 1;
+		} else {
+			return b*pow(b,e);
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println(calculateFaculty(4));
-
+		System.out.println(pow(2,3));
 	}
 
 }
